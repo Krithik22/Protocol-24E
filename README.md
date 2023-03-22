93e270ad819c0e2a516eb6b445da85fee84d1184
Krithik22 <krithiksaiprasad22@gmail.com>
2023-03-22 15:19:28 +0530

    website changed

--- README.md
+++
@@ -1,1 +0,0 @@
-# Protocol-24E
--- script.js
+++ script.js
@@ -1,13 +1,9 @@
 function openmenu(){
     document.getElementById("menu").style.visibility="visible";
     document.getElementById("menu").style.opacity=1;
-    // document.getElementById("menu").style.display="block";
-
 }
 
 function closemenu(){
     document.getElementById("menu").style.visibility="hidden";
     document.getElementById("menu").style.opacity=0;
-    // document.getElementById("menu").style.display="none";
-
 }
